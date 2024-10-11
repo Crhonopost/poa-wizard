6eddb8c18758125f7d27be14a6fbc95d6c9b7625
gubace <bonettim060@gmail.com>
2024-10-11 13:52:06 +0200

    ajout spells et classe environement

--- godot-wizard/Environement/generate.gd
+++ godot-wizard/Environement/generate.gd
@@ -8,7 +8,22 @@ var tile_id =  1
 
 #nombre tuiles aleatoires
 var tile_count = 60
+var spell_count = 4
 
+var tabWall = []
+var tabSpell = []
+
+func place_spell():
+	var placed_spell = 0
+	while placed_spell < spell_count :
+		var x = int(randf_range(-map_width+1, map_width-2))
+		var y = int(randf_range(-map_height-2, map_height))
+		var position = Vector2i(x, y)
+		
+		if get_cell_atlas_coords(0,position) == Vector2i(16,5):
+			tabSpell.append(position)
+			set_cell(0, position, 0,Vector2i(1, 1))
+			placed_spell += 1
 
 
 func place_random_tiles():
@@ -16,12 +31,14 @@ func place_random_tiles():
 	while placed_tiles < tile_count:
 		var x = int(randf_range(-map_width+1, map_width-2))
 		var y = int(randf_range(-map_height-2, map_height))
-		var position = Vector2(x, y)
+		var position = Vector2i(x, y)
+
 
 		# Vérifie si une tuile est déjà placée à cette position dans le layer foreground
 		if get_cell_atlas_coords(0,position) == Vector2i(16,5):
+			tabWall.append(position)
 			# Place la tuile à la position générée
-			set_cell(0, position, -1,Vector2i(1, 1))
+			set_cell(0, position, 0,Vector2i(15, 11))
 			placed_tiles += 1
 
 
@@ -30,7 +47,7 @@ func place_random_tiles():
 func _ready() -> void:
 	print(get_cell_atlas_coords(0,Vector2i(0, 0)))
 	place_random_tiles()
-
+	place_spell()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
